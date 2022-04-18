0579e58ef5c352566401d6faff505c3b3f2bdd7f
niwortma <nwortmann@icloud.com>
2022-04-18 13:55:14 +0200

    Revert "Revert "feat: add stocks/ticker screen""
    This reverts commit 62a80bed49984b41651fde7d11baf52901c0cdf6.

---
+++ pages/stocks/_ticker.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{ this.stock }}</div>
+</template>
+
+<script>
+import stocks from "~/data/stock-list";
+
+export default {
+  data() {
+    return {
+      stock: {},
+    };
+  },
+  created() {
+    const ticker = this.$route.params.ticker.toUpperCase();
+    const stock = stocks.find((stock) => stock.symbol === ticker);
+    this.stock = stock;
+  },
+};
+</script>
