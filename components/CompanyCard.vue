9319a0dd0aea2af6b6a8eb37f6b0d1eda270bf34
niwortma <nwortmann@icloud.com>
2022-04-18 13:24:56 +0200

    feat: add company card and index.vue

---
+++ components/CompanyCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card class="mx-auto" max-width="344" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-1">
+          {{ stock.name }}
+        </v-list-item-title>
+        <v-list-item-subtitle
+          >Type:
+          {{ this.capitalizeStockType(stock.type) }}</v-list-item-subtitle
+        >
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn outlined rounded text> {{ stock.symbol }} </v-btn>
+    </v-card-actions>
+  </v-card>
+
+  <!-- <v-card class="pa-2" outlined tile>
+    <h3>{{ stock.name }}</h3>
+    {{ stock.symbol }}<br />
+    {{ stock.price }}<br />
+  </v-card> -->
+</template>
+<script>
+export default {
+  props: {
+    stock: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    capitalizeStockType(type) {
+      return type === "etf"
+        ? type.toUpperCase()
+        : this.capitalizeFirstLetter(type);
+    },
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-container class="grey lighten-5">
+      <v-row no-gutters>
+        <v-col
+          v-for="stock in stocks"
+          :key="stock.symbol"
+          cols="12"
+          sm="4"
+          style="padding: 10px"
+        >
+          <company-card :stock="stock"></company-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import stocks from "~/data/stock-list";
+import CompanyCard from "~/components/CompanyCard.vue";
+
+export default {
+  components: {
+    CompanyCard,
+  },
+  data() {
+    return {
+      stocks: stocks.slice(0, 100),
+    };
+  },
+};
+</script>
+
+<style lang=""></style>
